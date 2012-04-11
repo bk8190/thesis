32d2fd0d4a852e25285a0bbb010c193caf3af55d
bk8190 <bk8190@gmail.com>
2012-04-11 12:25:50 -0400

    Removed text

---
+++ data/pan/plot_data.m
@@ -0,0 +1,13 @@
+clear all;close all;
+
+data = load('face_dataset_2');
+
+means = mean(data);
+norm(:,1) = data(:,1) - means(1);
+norm(:,2) = data(:,2) - means(2);
+
+figure(1)
+scatter(norm(:,1),norm(:,2))
+
+
+axis([-.5 .5 -.5 .5])
