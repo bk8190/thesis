89e4c7852a811bf7a1b56b9647f5d89d5e782b6f
bk8190 <bk8190@gmail.com>
2012-04-12 19:08:26 -0400

    stuff

--- data/pan/plot_data.m
+++ data/pan/plot_data.m
@@ -1,15 +1,39 @@
 clear all;close all;
 
-data = load('face_dataset_3');
+data{1} = load('face_dataset_no_ff_3')*100;
+data{2} = load('face_dataset_0.05_ff_2')*100;
 
-means = mean(data);
-norm(:,1) = data(:,1) - means(1);
-norm(:,2) = data(:,2) - means(2);
+% Remove outliers
+for i = 1:length(data)
+    d = data{i};
+    
+    inbounds = d(:,1) < 200;
+    data{i} = d(inbounds,:);
+end
+
+% Subtract off the mean
+for i = 1:length(data)
+    d = data{i};
+    
+    m = mean(d)
+%     d(:,1) = d(:,1) - m(1);
+    d(:,2) = d(:,2) - m(2);
+    
+    data{i} = d;
+end
+
+data1 = data{1};
+data2 = data{2};
 
 figure(1)
-scatter(norm(:,1),norm(:,2))
+hold on
+scatter(data1(:,2),data1(:,1),'b.')
+% scatter(data2(:,2),data2(:,1),'r.')
 
+xlim([-10 10])
+ylim([140 165])
+axis equal
 
-axis([-.5 .5 -.5 .5])
-xlabel('Side-side error (m)')
-ylabel('Front-back error (m)')
+ylabel('Distance from robot (cm)')
+xlabel('Lateral offset (cm)')
+title('World coordinates of detected face from panning Kinect')
